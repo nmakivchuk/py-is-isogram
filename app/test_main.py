306c2c87bb38a84a53146e1bbb1ca40b864dc4c3
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-09-24 19:58:26 +0300

    add iso program tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("word, expected", [
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("", True),
+    ("AaBbCc", False),
+    ("JavaScript", False),
+    ("Mississippi", False)
+])
+def test_is_isogram(word: str, expected:bool) -> None:
+    assert is_isogram(word) == expected
